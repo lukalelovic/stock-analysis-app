889c51c562499b7ca43d4a925a2d6ded79a630f2
lukalelovic <lukalelovic@gmail.com>
2024-08-04 18:16:06 -0500

    s&p 500 graph

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,56 @@
 from flask import render_template, flash, redirect, url_for, request
-import feedparser
 from app import app
+import yfinance as yf
+import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import feedparser
+import requests
 
 CNN_RSS = 'http://rss.cnn.com/rss/money_markets.rss'
 YAHOO_RSS = 'https://finance.yahoo.com/news/rssindex.rss'
 BLOOMBERG_RSS = 'https://feeds.bloomberg.com/markets/news.rss'
+REUTERS_RSS = 'https://www.reutersagency.com/feed/?best-topics=business-finance'
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  current_date = datetime.now()
+  seven_months_prior = current_date - relativedelta(months=7)
+
+  sp500 = yf.download('^GSPC', start=seven_months_prior.strftime('%Y-%m-%d'), end=current_date.strftime('%Y-%m-%d'))
+  sp500.reset_index(inplace=True)
+
+  data = sp500[['Date', 'Close']].to_dict(orient='records')
+
+  return render_template('index.html', data=data)
   
 @app.route('/cnn')
 def cnn():
-  return render_template('feed.html', news_title='CNN', logo='cnn.png', feed=fetch_rss_feed(CNN_RSS))
+  return render_template('feed.html', news_title='CNN', feed=fetch_rss_feed(CNN_RSS))
 
 @app.route('/yahoo')
 def yahoo():
-  return render_template('feed.html', news_title='Yahoo', logo='yahoo.png', feed=fetch_rss_feed(YAHOO_RSS))
+  return render_template('feed.html', news_title='Yahoo', feed=fetch_rss_feed(YAHOO_RSS))
 
 @app.route('/bloomberg')
 def bloomberg():
-  return render_template('feed.html', news_title='Bloomberg', logo='bloomberg.jpg', feed=fetch_rss_feed(BLOOMBERG_RSS))
+  return render_template('feed.html', news_title='Bloomberg', feed=fetch_rss_feed(BLOOMBERG_RSS))
+
+@app.route('/reuters')
+def reuters():
+  return render_template('feed.html', news_title='Reuters', feed=fetch_rss_feed(REUTERS_RSS))
 
 def fetch_rss_feed(url):
   entries = []
   feed = feedparser.parse(url)
+
+  if not feed.entries:
+    headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    xml = requests.get(url, headers=headers).text
+    xml = xml.replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "")
+    feed = feedparser.parse(xml)
 
   for entry in feed.entries:
     title = entry.title
